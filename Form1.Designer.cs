cefd7cd86730dfdda96f0b63ae40f76520217454
ShverdiakovMA <mshverdiakov@gmail.com>
2024-04-22 14:29:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Punkt2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnOpenFile = new System.Windows.Forms.Button();
+            this.BtnHoughCircle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 66);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 307);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnOpenFile
+            // 
+            this.BtnOpenFile.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BtnOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOpenFile.Location = new System.Drawing.Point(13, 19);
+            this.BtnOpenFile.Name = "BtnOpenFile";
+            this.BtnOpenFile.Size = new System.Drawing.Size(160, 40);
+            this.BtnOpenFile.TabIndex = 3;
+            this.BtnOpenFile.Text = "Open File";
+            this.BtnOpenFile.UseVisualStyleBackColor = false;
+            this.BtnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // BtnHoughCircle
+            // 
+            this.BtnHoughCircle.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BtnHoughCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHoughCircle.Location = new System.Drawing.Point(198, 19);
+            this.BtnHoughCircle.Name = "BtnHoughCircle";
+            this.BtnHoughCircle.Size = new System.Drawing.Size(160, 40);
+            this.BtnHoughCircle.TabIndex = 4;
+            this.BtnHoughCircle.Text = "Hough Circle";
+            this.BtnHoughCircle.UseVisualStyleBackColor = false;
+            this.BtnHoughCircle.Click += new System.EventHandler(this.BtnHoughCircle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(370, 387);
+            this.Controls.Add(this.BtnHoughCircle);
+            this.Controls.Add(this.BtnOpenFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnOpenFile;
+        private System.Windows.Forms.Button BtnHoughCircle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using Accord.Imaging;
+using Accord.Imaging.Filters;
+using Accord.Math.Geometry;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Punkt2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap ImgInput;
+        Bitmap grayImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Применяем подавление немаксимумов
+        private Bitmap ApplyNonMaximumSuppression(Bitmap gradientImage)
+        {
+            int width = gradientImage.Width;
+            int height = gradientImage.Height;
+            Bitmap result = new Bitmap(width, height);
+
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    Color pixel = gradientImage.GetPixel(x, y);
+
+                    double gradientMagnitude = CalculateGradientMagnitude(gradientImage, x, y);
+
+                    // Определяем направление градиента в градусах (0° - 180°)
+                    double gradientDirection = CalculateGradientDirection(gradientImage, x, y);
+
+                    // Проверяем направление градиента и подавляем немаксимумы
+                    bool isLocalMaximum = IsLocalMaximum(gradientImage, x, y, gradientMagnitude, gradientDirection);
+
+                    if (isLocalMaximum)
+                    {
+                        result.SetPixel(x, y, Color.FromArgb(255, 255, 255)); // Устанавливаем белый цвет для края
+                    }
+                    else
+                    {
+                        result.SetPixel(x, y, Color.FromArgb(0, 0, 0)); // Устанавливаем черный цвет для немаксимума
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        // Метод для вычисления модуля градиента в пикселе
+        private double CalculateGradientMagnitude(Bitmap gradientImage, int x, int y)
+        {
+            Color leftPixel = gradientImage.GetPixel(x - 1, y);
+            Color rightPixel = gradientImage.GetPixel(x + 1, y);
+            Color topPixel = gradientImage.GetPixel(x, y - 1);
+            Color bottomPixel = gradientImage.GetPixel(x, y + 1);
+
+            double gradientX = Math.Abs(leftPixel.R - rightPixel.R);
+            double gradientY = Math.Abs(topPixel.R - bottomPixel.R);
+
+            return Math.Sqrt(Math.Pow(gradientX, 2) + Math.Pow(gradientY, 2));
+        }
+
+        // Метод для вычисления направления градиента в пикселе в градусах (0° - 180°)
+        private double CalculateGradientDirection(Bitmap gradientImage, int x, int y)
+        {
+            Color leftPixel = gradientImage.GetPixel(x - 1, y);
+            Color rightPixel = gradientImage.GetPixel(x + 1, y);
+            Color topPixel = gradientImage.GetPixel(x, y - 1);
+            Color bottomPixel = gradientImage.GetPixel(x, y + 1);
+
+            double gradientX = rightPixel.R - leftPixel.R;
+            double gradientY = bottomPixel.R - topPixel.R;
+
+            return Math.Atan2(gradientY, gradientX) * (180 / Math.PI);
+        }
+
+        // Метод для проверки, является ли пиксель локальным максимумом
+        private bool IsLocalMaximum(Bitmap gradientImage, int x, int y, double gradientMagnitude, double gradientDirection)
+        {
+            double angle = gradientDirection % 180; // Приводим угол к диапазону [0, 180)
+
+            if ((angle >= 0 && angle < 22.5) || (angle >= 157.5 && angle < 180))
+            {
+                // Горизонтальные направления
+                return gradientMagnitude >= gradientImage.GetPixel(x - 1, y).R && gradientMagnitude >= gradientImage.GetPixel(x + 1, y).R;
+            }
+            else if (angle >= 22.5 && angle < 67.5)
+            {
+                // Диагональные направления (45°)
+                return gradientMagnitude >= gradientImage.GetPixel(x - 1, y - 1).R && gradientMagnitude >= gradientImage.GetPixel(x + 1, y + 1).R;
+            }
+            else if (angle >= 67.5 && angle < 112.5)
+            {
+                // Вертикальные направления
+                return gradientMagnitude >= gradientImage.GetPixel(x, y - 1).R && gradientMagnitude >= gradientImage.GetPixel(x, y + 1).R;
+            }
+            else
+            {
+                // Диагональные направления (135°)
+                return gradientMagnitude >= gradientImage.GetPixel(x + 1, y - 1).R && gradientMagnitude >= gradientImage.GetPixel(x - 1, y + 1).R;
+            }
+        }
+
+        private void BtnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ImgInput = new Bitmap(dialog.FileName);
+                pictureBox1.Image = ImgInput;
+            }
+        }
+
+        private void BtnHoughCircle_Click(object sender, EventArgs e)
+        {
+            Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap grayImage = grayscaleFilter.Apply(ImgInput);
+
+            Bitmap image = grayImage;
+
+            HoughCircleTransformation houghTransform = new HoughCircleTransformation(35);
+            houghTransform.ProcessImage(image);
+            Bitmap houghImage = houghTransform.ToBitmap();
+
+            HoughCircle[] circles = houghTransform.GetCirclesByRelativeIntensity(0.5); // Adjust the threshold as needed
+
+            houghImage = houghTransform.ToBitmap();
+
+            image.Save("./result.jpg");
+            pictureBox1.Image = houghImage;
+        }
+    }
+}
